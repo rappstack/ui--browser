cb378219b17a400a424cd15fb20d02e5b7588d5f
Brian Takita <brian.takita@gmail.com>
2024-03-24 21:35:10 -0400

    ./transition:
    	transition_tup_T→seq_034521_class_tup_T
    	transition_tupo_T→seq_034521_class_tupo_T
    	transition_tupo_→seq_034521_class_tupo_
    	transition_tup_→seq_034521_class_tup_
    	+ seq_034521_T
    	+ seq_034521_val_T
    	- transition_state_T
    	- transition_state_R_level

--- transition/index.ts
+++ transition/index.ts
@@ -1,88 +1,76 @@
-import { calling, memo_, type memo_T, sig_, sig_T } from 'ctx-core/rmemo'
+import { calling, memo_, type memo_T, sig_, type sig_T } from 'ctx-core/rmemo'
 import { el_setAttribute__set_memo } from '../el/index.js'
-export type transition_tup_T = [
-	enter_el__add:(el:Element)=>void,
-	transition_el__add:(el:Element)=>sig_T<transition_state_T>,
+export type seq_034521_class_tup_T = [
+	seq_enter_el__add:(el:Element)=>void,
+	seq_el__add:(el:Element)=>sig_T<seq_034521_val_T>,
 	enter$__add:(_enter$:memo_T<boolean>)=>void,
-	transition$__add:(_transition$:memo_T<boolean>)=>void,
+	seq$__add:(_transition$:memo_T<boolean>)=>void,
 	enter$:memo_T<boolean>,
-	transition$:memo_T<boolean>,
 ]
-export type transition_tupo_T = {
-	enter_el__add:(el:Element)=>void
-	transition_el__add:(el:Element)=>sig_T<transition_state_T>
-	enter$__add:(_enter$:memo_T<boolean>)=>void
-	transition$__add:(_transition$:memo_T<boolean>)=>void
-	enter$:memo_T<boolean>
-	transition$:memo_T<boolean>
-}&transition_tup_T
-export function transition_tupo_(enter$__new:(el:Element)=>memo_T<boolean>):transition_tupo_T {
-	const transition_o_key_R_transition_tup_idx = <Record<keyof transition_tupo_T, keyof transition_tup_T>>{
-		enter_el__add: 0,
-		transition_el__add: 1,
-		enter$__add: 2,
-		transition$__add: 3,
-		enter$: 4,
-		transition$: 5,
+export type seq_034521_class_tupo_T = {
+	seq_enter_el__add:(el:Element)=>void
+	seq_el__add:(el:Element)=>sig_T<seq_034521_val_T>
+	seq_enter$__add:(_enter$:memo_T<boolean>)=>void
+	seq$__add:(_transition$:memo_T<boolean>)=>void
+	seq_enter$:memo_T<boolean>
+}&seq_034521_class_tup_T
+export function seq_034521_class_tupo_(enter$__new:(el:Element)=>memo_T<boolean>):seq_034521_class_tupo_T {
+	const transition_o_key_R_transition_tup_idx = <Record<keyof seq_034521_class_tupo_T, keyof seq_034521_class_tup_T>>{
+		seq_enter_el__add: 0,
+		seq_el__add: 1,
+		seq_enter$__add: 2,
+		seq$__add: 3,
+		seq_enter$: 4,
 	}
-	return new Proxy(transition_tup_(enter$__new), {
+	return new Proxy(seq_034521_class_tup_(enter$__new), {
 		get(transition_tup, _prop) {
-			const prop = <keyof transition_tupo_T|keyof transition_tup_T>_prop
+			const prop = <keyof seq_034521_class_tupo_T|keyof seq_034521_class_tup_T>_prop
 			return (
 				transition_o_key_R_transition_tup_idx[prop] != null
 					? transition_tup[transition_o_key_R_transition_tup_idx[prop]]
 					: transition_tup[prop as number]
 			)
 		}
-	}) as transition_tupo_T
+	}) as seq_034521_class_tupo_T
 }
-export type transition_state_T = 'enter'|'enter-go'|'enter-done'|'leave'|'leave-go'|'leave-done'
-export const transition_state_R_level = <Record<transition_state_T, number>>{
-	'leave-done': -3,
-	'leave-go': -2,
-	leave: -1,
-	enter: 1,
-	'enter-go': 2,
-	'enter-done': 3
-}
-export function transition_tup_(enter$__new:(el:Element)=>memo_T<boolean>):transition_tup_T {
+export type seq_034521_T = [0, 3, 4, 5, 2, 1]
+export type seq_034521_val_T = seq_034521_T[number]
+const seq_034521 = <seq_034521_T>[0, 3, 4, 5, 2, 1]
+export function seq_034521_class_tup_(entry$__new:(el:Element)=>memo_T<boolean>):seq_034521_class_tup_T {
 	const enter$_a1$ = sig_<memo_T<boolean>[]>([])
 	const transition$_a1$ = sig_<memo_T<boolean>[]>([])
-	const enter$ = memo_(()=>enter$_a1$().some(enter$=>enter$()))
-	const transition$ = memo_(()=>transition$_a1$().some(transition$=>transition$()))
+	const seq_enter$ = memo_(()=>enter$_a1$().some(enter$=>enter$()))
 	return [
-		enter_el__add,
-		transition_el__add,
-		enter$__add,
-		transition$__add,
-		enter$,
-		transition$,
+		seq_enter_el__add,
+		seq_el__add,
+		seq_enter$__add,
+		seq$__add,
+		seq_enter$,
 	]
-	function enter_el__add(el:Element) {
-		enter$__add(enter$__new(el))
+	function seq_enter_el__add(el:Element) {
+		seq_enter$__add(entry$__new(el))
 	}
-	function enter$__add(_enter$:memo_T<boolean>) {
+	function seq_enter$__add(_enter$:memo_T<boolean>) {
 		enter$_a1$._ = [...enter$_a1$(), _enter$]
 	}
-	function transition_el__add(el:Element) {
-		const transition_state$ = sig_<transition_state_T>(enter$() ? 'enter-done' : 'leave-done')
-		transition$__add(transition$__new(el, transition_state$))
-		return transition_state$
+	function seq_el__add(el:Element) {
+		const seq_eq$ = sig_<seq_034521_val_T>(seq_enter$() ? 5 : 0)
+		seq$__add(seq$__new(el, seq_eq$))
+		return seq_eq$
 	}
-	function transition$__add(_transition$:memo_T<boolean>) {
+	function seq$__add(_transition$:memo_T<boolean>) {
 		transition$_a1$._ = [...transition$_a1$(), _transition$]
 	}
-	function transition$__new(_el:Element, transition_state$:sig_T<transition_state_T>) {
-		const el = <Element&{ transition_tup_effect$:memo_T<unknown> }>_el
-		el.transition_tup_effect$ = calling(memo_(()=>{
-			const level = transition_state_R_level[transition_state$()]
-			el.classList.add('listen')
-			el.classList.toggle('leave-done', level <= transition_state_R_level['leave-done'])
-			el.classList.toggle('leave-go', level <= transition_state_R_level['leave-go'])
-			el.classList.toggle('leave', level <= transition_state_R_level['leave'])
-			el.classList.toggle('enter', level >= transition_state_R_level['enter'])
-			el.classList.toggle('enter-go', level >= transition_state_R_level['enter-go'])
-			el.classList.toggle('enter-done', level >= transition_state_R_level['enter-done'])
+	function seq$__new(_el:Element, seq_eq$:sig_T<seq_034521_val_T>) {
+		const el = <Element&{ seq_eq$_effect$:memo_T<unknown> }>_el
+		el.seq_eq$_effect$ = calling(memo_(()=>{
+			const seq_eq = seq_eq$()
+			el.classList.add('seq-034521-on')
+			for (const seq of seq_034521) {
+				el.classList.toggle('seq-034521-eq-' + seq, seq_eq === seq)
+				el.classList.toggle('seq-034521-gte-' + seq, seq_eq >= seq)
+				el.classList.toggle('seq-034521-lt-' + seq, seq_eq < seq)
+			}
 		}))
 		return calling(memo_<boolean>($=>{
 				el.addEventListener('transitionstart', ()=>{
@@ -93,29 +81,29 @@ export function transition_tup_(enter$__new:(el:Element)=>memo_T<boolean>):trans
 				})
 				el.addEventListener('transitionend', ()=>{
 					$._ = false
-					if (transition_state$() === 'enter-go') {
-						transition_state$._ = 'enter-done'
-					} else if (transition_state$() === 'leave-go') {
-						transition_state$._ = 'leave-done'
+					if (seq_eq$() === 4) {
+						seq_eq$._ = 5
+					} else if (seq_eq$() === 1) {
+						seq_eq$._ = 0
 					}
 				})
 				return false
 			}).add(()=>{
 				return memo_<boolean>($=>{
-					const enter = enter$()
+					const enter = seq_enter$()
 					if ($.val != null) {
 						if (enter) {
 							if ($.val !== enter) {
-								transition_state$._ = 'enter'
+								seq_eq$._ = 3
 								setTimeout(()=>{
-									transition_state$._ = 'enter-go'
+									seq_eq$._ = 4
 								}, 30)
 							}
 						} else {
 							if ($.val !== enter) {
-								transition_state$._ = 'leave'
+								seq_eq$._ = 2
 								setTimeout(()=>{
-									transition_state$._ = 'leave-go'
+									seq_eq$._ = 1
 								}, 30)
 							}
 						}
